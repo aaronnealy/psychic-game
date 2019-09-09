bb08c85faa40d0d97658600959bf8cfc0fc7a4cd
Aaron Nealy <50381328+aaronnealy@users.noreply.github.com>
2019-09-09 17:25:48 -0500

    moved javascript code into game.js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+var wins = 0;
+    var lose = 0;
+    var guessCount = 5;
+    var guessChoices = [];
+    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"]
+
+    document.onkeyup = function(event) {
+
+        var userGuess = event.key;
+
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+        var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n" ,"o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"]
+
+        if(computerOptions.indexOf(userGuess) > -1){
+            if (userGuess === computerGuess){
+                wins++;
+                document.getElementById("wins").innerHTML = " Wins: " + wins;
+                guessCount = 5;
+                guessChoices = [];
+            }
+
+            if (userGuess != computerGuess){
+                guessCount --;
+                document.getElementById("guessesLeft").innerHTML = " Guesses Left: " + guessCount;
+                guessChoices.push(userGuess);
+                document.getElementById("guessesSF").innerHTML = " Your Guesses So Far: " + guessChoices;
+                
+            }
+
+            if (guessCount === 0) {
+                guessCount = 5;
+                lose ++;
+                document.getElementById("losses").innerHTML = " Losses: " + lose;
+                guessChoices = [];
+                
+            }
+
+
+			}
+
+        };
